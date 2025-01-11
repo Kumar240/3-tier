6d9fa3aaea3b48c0b8bee18d5c16f1c160c0f691
Kumar240 <110481802+Kumar240@users.noreply.github.com>
2025-01-11 14:42:21 +0530

    Update backend.tf

--- EKS-Cluster-TF/backend.tf
+++ EKS-Cluster-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "myaws060125"  # The S3 bucket to store the state file
     key    = "Terraform/terraform.tfstate"  # The path within the bucket (e.g., a folder structure)
     region = "ap-south-1"  # The AWS region where the bucket exists
-    dynamodb_table = "eks-cluster-terraform-lock-file-table"  # DynamoDB table for locking
+    dynamodb_table = "terraform-lock-table-lock-file-table"  # DynamoDB table for locking
     encrypt = true  # Encrypt the state file
     acl      = "bucket-owner-full-control"  # Permissions for the state file
   }
